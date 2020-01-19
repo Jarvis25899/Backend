5ed6b3e8b26000a0a32166f697f9fecdae3355ac
Jarvis25899 <35518754+Jarvis25899@users.noreply.github.com>
2020-01-19 18:14:40 +0530

    first commit

---
+++ src/main/java/com/example/CartAndOrder/CartAndOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.CartAndOrder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartAndOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartAndOrderApplication.class, args);
+	}
+
+}
